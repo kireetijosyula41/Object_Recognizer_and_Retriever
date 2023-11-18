3c166e98bef3a95e793f0f1122b77bb9a1840d2b
Jacob E Bilbro <jbilbro@csil5-20.cs.uchicago.edu>
2023-11-17 18:44:53 -0600

    arm_control skeleton improved

--- scripts/arm_control.py
+++ scripts/arm_control.py
@@ -14,11 +14,39 @@ import os
 
 class ActionHandler:
     def __init__(self):
-        ## Publishers and subscribers
+        #### Publishers and subscribers
         ## Subscribes to end_position topic, receives (x, y, z) coordinate of arm goal
-        self.end_pos_sub = rospy.Subscriber("end_position", HandPos, queue_size=10)
+        self.end_pos_sub = rospy.Subscriber("end_position", HandPos, self.set_end_pos, queue_size=10)
+        ## Interface to control arm movement
+        self.move_group_arm = moveit_commander.MoveGroupCommander("arm")
+        ## Interface to control arm gripper
+        self.move_group_gripper = moveit_commander.MoveGroupCommander("gripper")
 
 
+        #### Constants
+        ## Minimum and maximum r value for the coordinate system
+        self.r_min = 0.1
+        self.r_max = 0.8
+
+        ## Minimum and maximum theta (angle about x axis) value for the coordinate system
+        self.theta_min = 0.0
+        self.theta_max = (180.0) * (math.pi / 180) 
+
+        ## Minimum and maximum psi (angle about z axis) value for the coordinate system
+        self.theta_min = 0.0
+        self.theta_max = (360.0) * (math.pi / 180) 
+
+        ## Constants for arm dimensions (https://emanual.robotis.com/docs/en/platform/openmanipulator_x/specification/#dimension)
+        self.l1 = 0.130
+        self.l2 = 0.124
+        self.l3 = 0.126
+
+
+    ## Camera data should be converted to cartesian coordinates (x, y, z).
+    ## Based on the limitations of the arm movements, set a limited domain and set
+    ## coordinates outside of this domain to the nearest possible point.
+    def adjust_coordinates(self):
+        pass
 
     ## Main loop of execution
     def run(self):
